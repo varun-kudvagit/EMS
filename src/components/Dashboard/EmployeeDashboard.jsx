db653cac9cb19f0c2d10d06a805e95de7b7c50b7
varun-kudvagit <varunkudva111@gmail.com>
2025-02-28 21:09:05 +0530

    Add files via upload

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect } from 'react'
+import Header from '../../other/Header'
+import TaskListNumbers from '../../other/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
+import { AuthContext } from '../../context/AuthProvider'
+
+const EmployeeDashboard = ({ data, changeUser }) => {
+  const [userData, setUserData] = useContext(AuthContext)
+
+  useEffect(() => {
+    if (userData) {
+      const updatedEmployee = userData.find(emp => emp.id === data.id)
+      if (updatedEmployee) {
+        const updatedData = { role: "employee", data: updatedEmployee }
+        localStorage.setItem('loggedInUser', JSON.stringify(updatedData))
+      }
+    }
+  }, [userData, data.id])
+
+  const currentEmployeeData = userData?.find(emp => emp.id === data.id) || data
+
+  return (
+    <div className='min-h-screen bg-[#0a0a0a] p-6 md:p-10'>
+      <Header data={currentEmployeeData} changeUser={changeUser} />
+      <TaskListNumbers data={currentEmployeeData} />
+      <TaskList data={currentEmployeeData} />
+    </div>
+  )
+}
+
+export default EmployeeDashboard
